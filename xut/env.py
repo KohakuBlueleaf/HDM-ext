6e8ef9d129bd8654d34a01666f5a9422ceeae8b8
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-12-14 17:11:01 +0800

    update default xut env

--- xut/env.py
+++ xut/env.py
@@ -1,8 +1,8 @@
-TORCH_COMPILE = True
-USE_LIGER = True
+TORCH_COMPILE = False
+USE_LIGER = False
 USE_VANILLA = False
-USE_XFORMERS = True
-USE_XFORMERS_LAYERS = True
+USE_XFORMERS = False
+USE_XFORMERS_LAYERS = False
 COMPILE_ARGS = {
     "mode": "default",
     "dynamic": True,
